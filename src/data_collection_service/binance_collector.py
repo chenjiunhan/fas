05f50fea7e4e6a7b1dbd6a74ca6525a987e6b08b
JAQQ <chen.jiunhan@gmail.com>
2021-09-03 12:36:29 +0800

    init structure

---
+++ src/data_collection_service/binance_collector.py
@@ -0,0 +1,16 @@
+class BinanceCollector:
+    
+    def BinanceCollector(self):
+        pass
+
+    def collect_pair(self, symbol_1, symbol_2): #BTC/USDT
+        pass
+
+    def save_to_db(self, data, db_api):
+        pass
+
+    def run(self):
+        pass
+
+    def add_schedule(self, **params):
+        pass
