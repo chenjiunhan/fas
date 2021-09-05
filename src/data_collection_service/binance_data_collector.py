884dc04ccae59e13536e6cd8e2ac72a9bce13f3d
JAQQ <chen.jiunhan@gmail.com>
2021-09-05 15:56:21 +0800

    auto collect binance spot data

--- src/data_collection_service/binance_data_collector.py
+++ src/data_collection_service/binance_data_collector.py
@@ -1,15 +1,37 @@
+import requests
+
 class BinanceDataCollector:
     
     def BinanceDataCollector(self):
         pass
 
     @staticmethod
-    def collect_pair(symbol_1, symbol_2): #BTC/USDT
-        return f"collect_pair! {symbol_1}, {symbol_2}"
+    def collect_kline(symbol): #e.g. BTCUSDT
+        params = {
+            "symbol": symbol,
+            "interval": "1d",
+            "limit": 1000,
+        }
+        r = requests.get("https://api.binance.com/api/v3/klines", params=params)
+        
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return False
 
     @staticmethod
-    def save_to_db(self, data, db_api):
-        pass
+    def save_kline_to_db(symbol, kline):
+        data = {
+            "symbol": symbol,
+            "kline": kline
+        }
+        
+        r = requests.post("http://data_manager:10001/data_manager/binance/kline", json=data)
+
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return False
 
     def run(self):
         pass
--- src/data_collection_service/server.py
+++ src/data_collection_service/server.py
@@ -1,19 +1,74 @@
+import requests
+import schedule
+import threading
+import time
 import uvicorn
-from fastapi import FastAPI
+
 from binance_data_collector import BinanceDataCollector
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 
-class Pairs(BaseModel):
-    symbol_1: str
-    symbol_2: str
 
 app = FastAPI()
 binance_dc = BinanceDataCollector()
 
-@app.post("/data_collector/binance/pair_csv")
-def collect_pair(pairs: Pairs):
-    return binance_dc.collect_pair(pairs.symbol_1, pairs.symbol_2)
+class CollectKlineInput(BaseModel):
+    symbol: str
+
+@app.post("/data_collector/binance/kline")
+async def collect_kline(collect_kline_input: CollectKlineInput, response: Response):
+    kline = binance_dc.collect_kline(collect_kline_input.symbol)
+
+    if kline:
+        if binance_dc.save_kline_to_db(collect_kline_input.symbol, kline):
+            response_json = {
+                "success": True,
+                "message": "Save binance kline success!",
+                "status_code": 200
+            }
+        else:
+            response_json = {
+                "success": False,
+                "message": "Save to db failed!",
+                "status_code": 400
+            }
+    else:
+        response_json = {
+            "success": False,
+            "message": "Get binance kline failed!",
+            "status_code": 400
+        }
+
+    response.status_code = response_json["status_code"]
+    return response_json
+
+def cron():
+    schedule.every().day.at("00:00").do(request_collect_kline, symbol="BTCUSDT")
+    schedule.every().day.at("00:00").do(request_collect_kline, symbol="DOGEUSDT")
+    schedule.every().day.at("00:00").do(request_collect_kline, symbol="ETHBTC")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def request_collect_kline(symbol):
+    data = {
+        "symbol": symbol
+    }
+    
+    r = requests.post("http://data_collector:10000/data_collector/binance/kline", json=data)
+
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return False
 
 if __name__ == "__main__":
+
+    t = threading.Thread(target = cron)
+    t.daemon = True
+    t.start()
+
     uvicorn.run(app, host='0.0.0.0', 
                      port=10000, debug=True)
+
