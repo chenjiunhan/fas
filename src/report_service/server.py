80aad6b5e61569c8eef9a0685e0922b08e112593
JAQQ <chen.jiunhan@gmail.com>
2021-09-07 23:03:14 +0800

    improve reporter

--- src/report_service/server.py
+++ src/report_service/server.py
@@ -5,11 +5,23 @@ import time
 import uvicorn
 
 from reporter import Reporter
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+
+class Source:
+
+    def __init__(self, name, href, comment):
+        self.name = name
+        self.href = href
+        self.comment = comment
 
 app = FastAPI()
 reporter = Reporter()
+templates = Jinja2Templates(directory="templates")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 @app.get("/reporter/report")
 def report(response: Response):
@@ -24,6 +36,25 @@ def report(response: Response):
     
     response.status_code = response_json["status_code"]
     return response_json
+
+@app.get("/reporter/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+
+    sources = []
+    sources.append(Source("Crypto Market", "https://coinmarketcap.com/zh-tw/", "Observe crypto market changes"))
+    sources.append(Source("DappRadar", "https://dappradar.com/rankings", "Observe Dapp market"))
+    sources.append(Source("Opeasea NFT ranking", "https://opensea.io/rankings", "Find investment target"))
+
+    inputs = {
+        "request": request, 
+        "sources": sources
+    }
+
+    return templates.TemplateResponse("dashboard.html", inputs)
+
+@app.get("/reporter/tables", response_class=HTMLResponse)
+async def tables(request: Request):
+    return templates.TemplateResponse("tables.html", {"request": request})
 
 def cron():
     while True:
